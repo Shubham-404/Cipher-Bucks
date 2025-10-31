52f5224a9ee7c40c8c3ea369befe845f7dfe4bd3
Shubham-404 <shubh.shubhamkrsingh@gmail.com>
2025-10-31 20:14:53 +0530

    update sidepanel

--- client/src/components/SidePanel.jsx
+++ client/src/components/SidePanel.jsx
@@ -2,22 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const SidePanel = ({message, text}) => {
     return (
-        < div className="hidden lg:flex rounded-r-[50%] lg:w-1/2 bg-gradient-to-br bg-indigo-800 bg-[url(/images/background.png)] bg-no-repeat bg-bottom bg-contain bg-blend-color-burn  items-center justify-center p-12" >
-            <div className="text-white text-center">
-                <Link to="/" className="inline-block mb-8 hover:scale-105 transition-transform">
-                    <div className="flex items-center justify-center">
-                        <span className=''><img className='h-25 rounded-full' src="/images/logo-no-bg.png" alt="Vault Book Logo" /></span>
-                        <div className='h-15 mr-4 w-0.5 bg-white border'></div>
-                        <span className='flex flex-col justify-center items-start text-3xl font-semibold'>
-                            <span>Vault</span>
-                            <span>Book</span>
-                        </span>
-                    </div>
-                </Link>
-                <h1 className="text-3xl font-bold mb-4">{message}</h1>
-                <p className="text-xl mb-8">{text}</p>
-                <p className="text-lg text-blue-100">Simple • Secure • Encrypted</p>
-            </div>
+        < div className="lg:flex max-lg:absolute top-1/2 left-1/2 max-lg:h-full w-full max-lg:-translate-1/2 lg:w-1/2 bg-[url(/images/bg-2.png)] bg-no-repeat bg-center bg-contain saturate-120 dark:opacity-80 items-center justify-center" >
+            
         </div >
     )
 }
